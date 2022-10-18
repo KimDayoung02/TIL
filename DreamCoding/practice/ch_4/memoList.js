ad146d81af3227f47da9bc6be5168994f6272de6
KimDayoung <dayoung7833@gmail.com>
2022-10-18 17:39:41 +0900

    챕터4 실습 프로젝트1 - Todo리스트 만들기 (완성)
    메모추가, 메모 내용 완료, 메모삭제 기능 추가 완료!

--- DreamCoding/practice/ch_4/memoList.js
+++ DreamCoding/practice/ch_4/memoList.js
@@ -1,1 +1,38 @@
-let memo = document.querySelector(".memo");
+let memoText = document.querySelector(".memoAddText");
+let memoAddButtom = document.querySelector(".memoAddButton");
+let memoList = document.querySelector(".memoList");
+
+memoAddButtom.addEventListener("click", () => {
+  if (!memoText.value) {
+    alert("메모할 내용이 없습니다! 내용을 입력해주세요!");
+  } else {
+    let checkMark = document.createElement("span");
+    checkMark.innerHTML = ` ✔ `;
+    checkMark.style.fontSize = "25px";
+    checkMark.style.cursor = "pointer";
+    checkMark.style.color = "green";
+
+    let removeMark = document.createElement("span");
+    removeMark.innerHTML = ` X <br>`;
+    removeMark.style.fontSize = "25px";
+    removeMark.style.cursor = "pointer";
+    removeMark.style.color = "red";
+
+    let memoListText = document.createElement("h2");
+    memoListText.innerHTML = memoText.value;
+
+    memoList.appendChild(memoListText);
+    memoListText.appendChild(checkMark);
+    memoListText.appendChild(removeMark);
+
+    memoText.value = "";
+
+    checkMark.addEventListener("click", () => {
+      checkMark.parentElement.style.textDecorationLine = "line-through";
+    });
+
+    removeMark.addEventListener("click", () => {
+      removeMark.parentElement.remove();
+    });
+  }
+});
