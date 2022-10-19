fac3c05280b74b60288e26a8b5bedcd0b88cf313
KimDayoung02 <dayoung7833@gmail.com>
2022-10-19 17:00:22 +0900

    ToDo 리스트 수정 : 일정량 메모가 추가되면 스크롤, 엔터로 메모등록
    일정량 메모가 쌓이면 스크롤로 표현돼서 내려가고 엔터로도 메모가 추가될 수 있도록 수정

--- DreamCoding/browser101/ch_4/memoList.js
+++ DreamCoding/browser101/ch_4/memoList.js
@@ -8,53 +8,77 @@ window.addEventListener("resize", () => {
   html.style.height = window.outerHeight;
 });
 
-memoAddButtom.addEventListener("click", () => {
+// 보통 이벤트 함수들은 이름 앞에 on을 붙여서 사용
+
+const onAdd = () => {
   if (!memoText.value) {
     alert("메모할 내용이 없습니다! 내용을 입력해주세요!");
-  } else {
-    let checkMark = document.createElement("span");
-    checkMark.innerHTML = ` ✔ `;
-    checkMark.style.fontSize = "25px";
-    checkMark.style.cursor = "pointer";
+    memoText.focus();
+    return;
+  }
+  // createItem
+  let checkMark = document.createElement("span");
+  checkMark.innerHTML = ` ✔ `;
+  checkMark.style.fontSize = "25px";
+  checkMark.style.cursor = "pointer";
+  checkMark.style.color = "green";
+
+  let removeMark = document.createElement("span");
+  removeMark.innerHTML = ` X <br>`;
+  removeMark.style.fontSize = "25px";
+  removeMark.style.cursor = "pointer";
+  removeMark.style.color = "firebrick";
+
+  /*이렇게 일일히 style로 CSS 지정해주지 않아도 check와 remove를 
+    setAttribute로 id값을 각각 지정해 css파일에서 해줘도 됨*/
+
+  let memoListText = document.createElement("h2");
+  memoListText.innerHTML = memoText.value;
+  //아무래도 리스트니까 ul, li태그를 쓰는 것도 좋았을 듯
+
+  memoList.appendChild(memoListText);
+  memoListText.appendChild(checkMark);
+  memoListText.appendChild(removeMark);
+
+  memoListText.scrollIntoView({ block: "center" });
+
+  memoText.value = "";
+  memoText.focus();
+
+  //onMouseHover
+  checkMark.onmouseover = function () {
+    checkMark.style.color = "rgb(125, 209, 125)";
+    checkMark.style.transition = "all 0.1s ease-in";
+  };
+  checkMark.onmouseout = function () {
     checkMark.style.color = "green";
-
-    let removeMark = document.createElement("span");
-    removeMark.innerHTML = ` X <br>`;
-    removeMark.style.fontSize = "25px";
-    removeMark.style.cursor = "pointer";
+  };
+  removeMark.onmouseover = function () {
+    removeMark.style.color = "red";
+    removeMark.style.transition = "all 0.1s ease-in";
+  };
+  removeMark.onmouseout = function () {
     removeMark.style.color = "firebrick";
+  };
 
-    let memoListText = document.createElement("h2");
-    memoListText.innerHTML = memoText.value;
-    //아무래도 리스트니까 ul, li태그를 쓰는 것도 좋았을 듯
+  //onCheckedItem
+  checkMark.addEventListener("click", () => {
+    checkMark.parentElement.style.textDecorationLine = "line-through";
+    checkMark.parentElement.style.color = "rgba(128, 128, 128, 0.57)";
+  });
 
-    memoList.appendChild(memoListText);
-    memoListText.appendChild(checkMark);
-    memoListText.appendChild(removeMark);
+  //onRemoveItem
+  removeMark.addEventListener("click", () => {
+    removeMark.parentElement.remove();
+  });
+};
 
-    memoText.value = "";
-    checkMark.onmouseover = function () {
-      checkMark.style.color = "rgb(125, 209, 125)";
-      checkMark.style.transition = "all 0.1s ease-in";
-    };
-    checkMark.onmouseout = function () {
-      checkMark.style.color = "green";
-    };
-    removeMark.onmouseover = function () {
-      removeMark.style.color = "red";
-      removeMark.style.transition = "all 0.1s ease-in";
-    };
-    removeMark.onmouseout = function () {
-      removeMark.style.color = "firebrick";
-    };
+memoAddButtom.addEventListener("click", () => {
+  onAdd();
+});
 
-    checkMark.addEventListener("click", () => {
-      checkMark.parentElement.style.textDecorationLine = "line-through";
-      checkMark.parentElement.style.color = "rgba(128, 128, 128, 0.57)";
-    });
-
-    removeMark.addEventListener("click", () => {
-      removeMark.parentElement.remove();
-    });
+memoText.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    onAdd();
   }
 });
