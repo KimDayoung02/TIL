e129217007bb92f6d1a642ce65c03016e6f60e87
KimDayoung02 <dayoung7833@gmail.com>
2022-10-21 23:26:05 +0900

    게임만들기 실습 - 나비잡기 : 시작 전 설명추가, 시작 클릭 시 랜덤배치
    시작 전 설명을 추가하였고 시작 버튼을 누르면 gameZone에 무작위로 나비와 말벌들이 10개씩 배치되게 함.
    추가로 시작버른을 누르면 정지버튼으로 바뀌게 함

--- DreamCoding/browser101/ch_5/game.js
+++ DreamCoding/browser101/ch_5/game.js
@@ -1,7 +1,8 @@
 // let hornet = document.getElementsByClassName("hornet");
-let hornet = document.querySelectorAll(".hornet");
-let butterfly = document.querySelectorAll(".butterfly");
+// let hornet = document.querySelectorAll(".hornet");
+// let butterfly = document.querySelectorAll(".butterfly");
 let gameZone = document.querySelector("#gameZone");
+let startBtn = document.querySelector(".startBtn");
 //여기서 getElements~는 HTMLCollection으로 갖고오고 querySelector는 NodeList로 받아오는 걸 확인할 수 있었는데
 //HTMLCollection은 map, forEach 둘 다 안먹히고 NodeList는 map은 안먹히지만 forEach는 먹혔다.
 
@@ -11,6 +12,23 @@ let gameZone = document.querySelector("#gameZone");
 //   hornet[i].style.top = "600px";
 // }
 
+function createitems(num) {
+  for (let i = 0; i < num; i++) {
+    let butterfly = document.createElement("div");
+    butterfly.setAttribute("class", "butterfly");
+    butterfly.innerHTML = `
+    <img src="gameImg/ButterFly.png" alt="나비그림">`;
+    gameZone.appendChild(butterfly);
+
+    let hornet = document.createElement("div");
+    hornet.setAttribute("class", "hornet");
+    hornet.innerHTML = `
+    <img src="gameImg/Hornet.png" alt="말벌그림">`;
+    gameZone.appendChild(hornet);
+    itemsRandomPlacement(butterfly, hornet);
+  }
+}
+
 function getRandomCoordinate(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -18,37 +36,30 @@ function getRandomCoordinate(min, max) {
   //최댓값은 제외, 최솟값은 포함됨
 }
 
-function itemsRandomPlacement() {
+function itemsRandomPlacement(butterfly, hornet) {
   let coordinate = gameZone.getBoundingClientRect();
-  let gameZoneMaxX = coordinate.right - 40;
-  let gameZoneMinX = coordinate.left + 40;
-  let gameZoneYMaxY = coordinate.bottom - coordinate.top - 40;
-  let gameZoneYMinY = coordinate.top + 40;
-  console.log(coordinate);
-  console.log(gameZoneYMinY, gameZoneYMaxY);
-  hornet.forEach((hornet) => {
-    hornet.style.top = `${getRandomCoordinate(
-      gameZoneYMinY + 40,
-      gameZoneYMaxY - 40
-    )}px`;
-    hornet.style.left = `${getRandomCoordinate(
-      gameZoneMinX + 40,
-      gameZoneMaxX - 40
-    )}px`;
-  });
-  butterfly.forEach((btf) => {
-    btf.style.top = `${getRandomCoordinate(
-      gameZoneYMinY + 40,
-      gameZoneYMaxY - 40
-    )}px`;
-    btf.style.left = `${getRandomCoordinate(
-      gameZoneMinX + 40,
-      gameZoneMaxX - 40
-    )}px`;
-  });
+  let gameZoneMaxX = coordinate.right - 80;
+  let gameZoneMinX = coordinate.left;
+  let gameZoneYMaxY = coordinate.bottom - coordinate.top - 80;
+  let gameZoneYMinY = coordinate.top;
+
+  hornet.style.top = `${getRandomCoordinate(gameZoneYMinY, gameZoneYMaxY)}px`;
+  hornet.style.left = `${getRandomCoordinate(gameZoneMinX, gameZoneMaxX)}px`;
+  butterfly.style.top = `${getRandomCoordinate(
+    gameZoneYMinY,
+    gameZoneYMaxY
+  )}px`;
+  butterfly.style.left = `${getRandomCoordinate(gameZoneMinX, gameZoneMaxX)}px`;
 }
 
-itemsRandomPlacement();
-gameZone.addEventListener("click", (e) => {
-  console.log(e.clientY);
+startBtn.addEventListener("click", () => {
+  let isExplain = document.querySelector(".explain");
+  if (isExplain) {
+    isExplain.remove();
+  }
+  createitems(10);
+  startBtn.innerHTML = `<i class="fa-sharp fa-solid fa-stop"></i>`;
+  startBtn.addEventListener("change", (e) => {
+    console.log("play!->stop!");
+  });
 });
