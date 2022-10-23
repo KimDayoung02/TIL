4bc5c12623eb238e817d54430b6b93f3fb20b208
KimDayoung02 <dayoung7833@gmail.com>
2022-10-23 23:19:11 +0900

    게임만들기 실습 - 나비잡기 : 난이도 조정, 나비 잡기 동작
    나비를 잡으면 남은 나비의 수가 줄며 나비가 없어지는 것을 추가 하였다. 추가로 난이도 조정을 위해 말벌의 수는 나비의 절반으로 나타나게 수정

--- DreamCoding/browser101/ch_5/game.js
+++ DreamCoding/browser101/ch_5/game.js
@@ -14,24 +14,25 @@ let startBtn = document.querySelector(".startBtn");
 let timer = document.querySelector(".timer");
 let butterflyCount = document.querySelector(".butterflyCount");
 
-let btfCount = 10;
+let btfCount = 15;
 butterflyCount.innerHTML = `${btfCount}`;
 
 function createitems(num) {
   for (let i = 0; i < num; i++) {
-    let hornet = document.createElement("div");
-    if (i % 3 == 0) {
+    if (i % 2 == 0) {
+      let hornet = document.createElement("div");
       hornet.setAttribute("class", "hornet");
       hornet.innerHTML = `
     <img src="gameImg/Hornet.png" alt="말벌그림">`;
       gameZone.appendChild(hornet);
+      itemsRandomPlacement(hornet);
     }
     let butterfly = document.createElement("div");
     butterfly.setAttribute("class", "butterfly");
     butterfly.innerHTML = `
     <img src="gameImg/ButterFly.png" alt="나비그림">`;
     gameZone.appendChild(butterfly);
-    itemsRandomPlacement(butterfly, hornet);
+    itemsRandomPlacement(butterfly);
   }
 }
 
@@ -42,20 +43,15 @@ function getRandomCoordinate(min, max) {
   //최댓값은 제외, 최솟값은 포함됨
 }
 
-function itemsRandomPlacement(butterfly, hornet) {
+function itemsRandomPlacement(item) {
   let coordinate = gameZone.getBoundingClientRect();
   let gameZoneMaxX = coordinate.right - 80;
   let gameZoneMinX = coordinate.left;
   let gameZoneYMaxY = coordinate.bottom - coordinate.top - 80;
   let gameZoneYMinY = coordinate.top;
 
-  hornet.style.top = `${getRandomCoordinate(gameZoneYMinY, gameZoneYMaxY)}px`;
-  hornet.style.left = `${getRandomCoordinate(gameZoneMinX, gameZoneMaxX)}px`;
-  butterfly.style.top = `${getRandomCoordinate(
-    gameZoneYMinY,
-    gameZoneYMaxY
-  )}px`;
-  butterfly.style.left = `${getRandomCoordinate(gameZoneMinX, gameZoneMaxX)}px`;
+  item.style.top = `${getRandomCoordinate(gameZoneYMinY, gameZoneYMaxY)}px`;
+  item.style.left = `${getRandomCoordinate(gameZoneMinX, gameZoneMaxX)}px`;
 }
 
 startBtn.addEventListener("click", () => {
@@ -69,22 +65,38 @@ startBtn.addEventListener("click", () => {
 
   let butterfly = document.querySelectorAll(`.butterfly`);
   let hornet = document.querySelectorAll(".hornet");
-  // catchItem(butterfly, hornet);
+  catchItem(butterfly, hornet);
 });
 
 function timeStart() {
   let time = 0;
   let min = "";
   let sec = "";
+  let mmsec = "";
 
   setInterval(() => {
-    min = parseInt(time / 60);
+    mmsec = time % 10;
+    sec = parseInt((time / 10) % 60);
 
-    sec = time % 60;
+    min = parseInt(time / 600);
     timer.innerHTML = `${min < 10 ? `0${min}` : min} : ${
       sec < 10 ? `0${sec}` : sec
-    }`;
+    } . ${mmsec}`;
 
     time++;
-  }, 10);
+  }, 100);
+}
+function catchItem(butterfly, hornet) {
+  butterfly.forEach((btf) => {
+    btf.addEventListener("click", () => {
+      btf.remove();
+      btfCount--;
+      butterflyCount.innerHTML = `${btfCount}`;
+    });
+  });
+  hornet.forEach((hor) => {
+    hor.addEventListener("click", () => {
+      console.log("Game Over!");
+    });
+  });
 }
