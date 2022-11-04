110de2b0bab67151e95600f0f18f4b5c2326809d
KimDayoung02 <dayoung7833@gmail.com>
2022-11-04 11:43:48 +0900

    ch_3 정렬 알고리즘 : 버블정렬 성능 향상 (noSwap)
    반복문에서 정렬을 위한 교환이 한번도 일어나지 않은 경우 정렬이 모두 되었다고 판단하고 반복을 멈추기 위한 noSwap(boolean)을 추가함

--- Udemy/Algorithm_JS/ch_3 정렬알고리즘/bubbleSort.js
+++ Udemy/Algorithm_JS/ch_3 정렬알고리즘/bubbleSort.js
@@ -11,4 +11,24 @@ function bubbleSort(array) {
     console.log("loop end!");
   }
 }
-bubbleSort([10, 12, 13, 50, 20, 8, 6, 32, 1]);
+// bubbleSort([10, 12, 13, 50, 20, 8, 6, 32, 1]);
+
+//정렬이 거의 다 됐을 경우 쓸데없는 반복은 피하기 위해 noSwap을 추가
+function bubbleSortWithNoSwap(array) {
+  let noSwap;
+  for (let i = array.length; i > 1; i--) {
+    noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        noSwap = false;
+      }
+      console.log(array);
+    }
+    console.log("loop end!");
+    if (noSwap) break;
+  }
+}
+bubbleSortWithNoSwap([1, 2, 3, 4, 5, 8, 6, 7]);
