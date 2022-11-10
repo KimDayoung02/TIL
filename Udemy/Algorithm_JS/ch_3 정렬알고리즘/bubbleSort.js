214bec61000d2dcb0474bc956a27e0685f9a754f
KimDayoung02 <dayoung7833@gmail.com>
2022-11-10 14:55:55 +0900

    퀵정렬 swap 함수 따로 빼기, 소요시간 알아보기
    소요시간을 알아보기위해 코드를 추가하였음

--- Udemy/Algorithm_JS/ch_3 정렬알고리즘/bubbleSort.js
+++ Udemy/Algorithm_JS/ch_3 정렬알고리즘/bubbleSort.js
@@ -14,6 +14,7 @@ function bubbleSort(array) {
 // bubbleSort([10, 12, 13, 50, 20, 8, 6, 32, 1]);
 
 //정렬이 거의 다 됐을 경우 쓸데없는 반복은 피하기 위해 noSwap을 추가
+//한번도 교환이 일어나지 않았을 경우 정렬이 모두 되었다고 판단하는 것
 function bubbleSortWithNoSwap(array) {
   let noSwap;
   for (let i = array.length; i > 1; i--) {
@@ -25,10 +26,18 @@ function bubbleSortWithNoSwap(array) {
         array[j + 1] = temp;
         noSwap = false;
       }
-      console.log(array);
     }
-    console.log("loop end!");
     if (noSwap) break;
   }
+  return array;
 }
-bubbleSortWithNoSwap([1, 2, 3, 4, 5, 8, 6, 7]);
+let arr = [
+  50, 12, 6, 235, 9, 5, 6, 754, 32, 5, 62, 4, 8, 15, 51, 15, 15, 478, 6, 5, 23,
+  54, 21, 563, 321, 5, 4, 85, 69, 5, 6, 778, 12,
+];
+console.time("Performance Time"); // 시작시간
+bubbleSortWithNoSwap(arr);
+
+console.timeEnd("Performance Time"); // 해당 부분에서 콘솔로 값을 리턴 받는다.
+
+console.log(arr);
--- Udemy/Algorithm_JS/ch_5 퀵정렬/quickSort.js
+++ Udemy/Algorithm_JS/ch_5 퀵정렬/quickSort.js
@@ -1,26 +1,34 @@
+function swap(arr, a, b) {
+  let temp;
+  temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
 function quickSort(arr, first, last) {
   let pivot = first;
-  let temp;
+
   if (first >= last) return arr;
   for (let i = first; i <= last; i++) {
     if (arr[first] > arr[i]) {
       pivot++;
-      temp = arr[i];
-      arr[i] = arr[pivot];
-      arr[pivot] = temp;
+      swap(arr, i, pivot);
       //   console.log(arr, first, last);
     }
   }
-  temp = arr[first];
-  arr[first] = arr[pivot];
-  arr[pivot] = temp;
+  swap(arr, first, pivot);
 
   quickSort(arr, first, pivot - 1);
   quickSort(arr, pivot + 1, arr.length - 1);
   return arr;
 }
 
-let array = [50, 12, 6, 235, 9, 5, 6, 754, 32, 5, 62, 4, 8, 15];
+let array = [
+  50, 12, 6, 235, 9, 5, 6, 754, 32, 5, 62, 4, 8, 15, 51, 15, 15, 478, 6, 5, 23,
+  54, 21, 563, 321, 5, 4, 85, 69, 5, 6, 778, 12,
+];
+
+console.time("Performance Time"); // 시작시간
 quickSort(array, 0, array.length - 1);
+console.timeEnd("Performance Time"); // 해당 부분에서 콘솔로 값을 리턴 받는다.
 
 console.log(array);
